c8e58681ecdb70b9e3098280ec9a9e7eace251ea
AustinBinge <Austin@bingetech.com>
2014-01-22 20:32:45 -0700

    Edits from today

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 		<script type="text/javascript">
 		<!--
-			resize();
+			resize(//);
 		//--></script>
 		<footer>
 			
--- single.php
+++ single.php
@@ -17,10 +17,7 @@ get_header(); ?>
 					 */
 					get_template_part( 'content', get_post_format() );
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
+					
 				endwhile;
 			?>
 		</div><!-- #content -->
